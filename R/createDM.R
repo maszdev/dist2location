0f1aa2db88516e0ac88ca312589fd05cd3e24923
maszdev <maszdev@users.noreply.github.com>
2021-01-19 20:22:28 +0100

    create distance matrix completed

--- R/createDM.R
+++ R/createDM.R
@@ -32,17 +32,25 @@
 #' When input data frame is not complete (not all possible distances are
 #' described) missing cases will be marked in distance matrix as NA.
 #'
-#' create_distance_matrix secures first and second metrics rule (see vignette
+#' create_distance_matrix() secures first and second metrics rule (see vignette
 #' for package) so even if this info is not included in input df output dm
 #' will include 0 in diagonal.
-#' When there are many (different) distances for the same points in df, output
-#' dm will include only last (for both (i,j) and (j,i) cases - second metrics
-#' rule fulfilled)
+#' When there are many (different) distances for the same points in data frame,
+#' output distance matrix will include only last (for both (i,j) and (j,i)
+#' cases - so second metrics rule is fulfilled).
+#' @param df data frame including cases (point1,point2,distance)
+#' @return distance matrix
+#'
 #' @export
 create_distance_matrix <- function(df) {
   df_format <- validate_df(df)
+  if (!(df_format %in% c(1,2)))
+    stop("Input data frame format not recognized properly")
 
+  if(df_format == 1) create_dm_from_df_format_1(df)
+  else create_dm_from_df_format_2(df)  #Format2
 }
+
 #' Function validates data frame with distance cases (point1,point2,distance)
 #' \itemize{
 #' \item If values included in all three columns are numeric 1 is returned
@@ -57,11 +65,51 @@ validate_df <- function(df) {
   if(!is.numeric(df[,3])) stop("Third column should include numeric values")
 
   format <- 0
-  if(is.numeric(df[1,]) & is.numeric(df[2,])) format <- 1
-  if(is.character(df[1,]) & is.character(df[2,])) format <- 2
+  if(is.numeric(df[,1]) & is.numeric(df[,2])) format <- 1
+  if(is.character(df[,1]) & is.character(df[,2])) format <- 2
   if (format == 0) stop(paste("First two columns should include numeric values",
                        "or first two columns should include character values"))
 
   return(format)
+}
+
+#' Function takes as an input data frame prepared in format 1
+#' (column types - numeric, numeric, numeric) and returns distance matrix
+#'
+create_dm_from_df_format_1 <- function(df) {
+  df[,1] <- as.integer(df[,1])
+  df[,2] <- as.integer(df[,2])
+  size <- max(c(df[,1],df[,2]))
+
+  m <- matrix(data=NA,nrow=size,ncol=size)
+  for(i in 1:nrow(df)) {
+    m[df[i,1],df[i,2]] <- df[i,3]
+    m[df[i,2],df[i,1]] <- df[i,3]
+  }
+
+  for(i in 1:size) m[i,i] <- 0
+
+  return(m)
+}
+
+#' Function takes as an input data frame prepared in format 2
+#' (column types - character, character, numeric) and returns distance matrix
+#'
+create_dm_from_df_format_2 <- function(df) {
+  all_names <- sort(unique(c(df[,1],df[,2])))
+  size <- length(all_names)
+
+  m <- matrix(data=NA,nrow=size,ncol=size)
+  colnames(m) <- all_names
+  rownames(m) <- all_names
+
+  for(i in 1:nrow(df)) {
+    m[match(df[i,1],all_names),match(df[i,2],all_names)] <- df[i,3]
+    m[match(df[i,2],all_names),match(df[i,1],all_names)] <- df[i,3]
+  }
+
+  for(i in 1:size) m[i,i] <- 0
+
+  return(m)
 }
 
--- tests/testthat/test_createDM.R
+++ tests/testthat/test_createDM.R
@@ -1,5 +1,7 @@
 context("Create Distance Matrix")
 
+# DATA FOR VALIDATION TESTS
+
 m <- m_ok <- matrix(c(0,1,1,
                       1,0,1,
                       1,1,0),3,3)
@@ -19,6 +21,81 @@ df_wrong3 <- data.frame (first_column  = c(1,2,3),
 
 df_wrong4 <- data.frame (first_column  = c(1,2,3),
                          second_column = c(2, 3,1))
+
+# DATA FOR FORMAT1 TESTS
+
+df_format1 <- data.frame (first_column  = c(1,3,1),
+                          second_column = c(2, 2,3),
+                          third_column = c(2.1, 7.3,4))
+
+df_format1_index_double <- data.frame (first_column  = c(1.1,3,1),
+                          second_column = c(2, 2.9,3),
+                          third_column = c(2.1, 7.3,4))
+
+#one point pairs repeated twice (with different distance)
+#one distance between the same point not equal to 0
+df_format1_with_additions <- data.frame (first_column  = c(3,1,3,1,2),
+                          second_column = c(1,2, 2,3,2),
+                          third_column = c(10,2.1, 7.3,4,1))
+
+df_format1_uncompleted <- data.frame (first_column  = c(1,3,1),
+                                      second_column = c(2,2,4),
+                                      third_column = c(2.1,7.3,4))
+
+df_format1_uncompleted2 <- data.frame(first_column  = c(1),
+                                    second_column = c(4),
+                                    third_column = c(2))
+
+
+expected_m_1 <- matrix(c(0,2.1,4,
+                       2.1,0,7.3,
+                       4,7.3,0),3,3)
+
+
+expected_m_uncomplited <- matrix(c(0,2.1,NA,4,
+                                  2.1,0,7.3,NA,
+                                  NA,7.3,0,NA,
+                                  4,NA,NA,0),4,4)
+
+expected_m_uncomplited2 <- matrix(c(0,NA,NA,2,
+                                    NA,0,NA,NA,
+                                    NA,NA,0,NA,
+                                    2,NA,NA,0),4,4)
+
+
+# DATA FOR FORMAT2 TESTS
+df_format2 <- data.frame (first_column  = c("A","C","A"),
+                          second_column = c("B", "B","C"),
+                          third_column = c(2.1, 7.3,4))
+
+df_format2_uncompleted <- data.frame (first_column  = c("C","A","B","D"),
+                                      second_column = c("D","B","C","A"),
+                                      third_column = c(2,3,4,5))
+
+df_format2_with_additions <- data.frame (first_column  = c("C","A","C","A","A"),
+                                      second_column = c("B", "B", "B","C","A"),
+                                      third_column = c(11,2.1, 7.3,4,12))
+
+expected_m_2 <- matrix(c(0,2.1,4,
+                         2.1,0,7.3,
+                         4,7.3,0),3,3)
+
+colnames(expected_m_2) <- c("A","B","C")
+rownames(expected_m_2) <- c("A","B","C")
+
+expected_m2_uncompleted <- matrix(c(0,3,NA,5,
+                                    3,0,4,NA,
+                                    NA,4,0,2,
+                                    5,NA,2,0),4,4)
+
+colnames(expected_m2_uncompleted) <- c("A","B","C","D")
+rownames(expected_m2_uncompleted) <- c("A","B","C","D")
+
+
+
+# VALIDATION
+#
+
 
 test_that("Not a data frame", {
   expect_error(create_distance_matrix(m),
@@ -44,4 +121,52 @@ test_that("Wrong format, to many columns", {
 test_that("Wrong format, not enough columns", {
   expect_error(create_distance_matrix(df_wrong4),
                "Data frame should have 3 columns")
+})
+
+test_that("Validate, format 1", {
+  expect_equal(validate_df(df_format1), 1)
+})
+
+test_that("Validate, format 1, double instead of integer", {
+  expect_equal(validate_df(df_format1_index_double), 1)
+})
+
+
+## TEST FOR FORMAT 1
+
+test_that("Matrix, format 1", {
+  expect_equal(create_distance_matrix(df_format1), expected_m_1)
+})
+
+test_that("Matrix, format 1, indexes has double type", {
+  expect_equal(create_distance_matrix(df_format1_index_double), expected_m_1)
+})
+
+test_that("Matrix, format 1, not needed info", {
+  expect_equal(create_distance_matrix(df_format1_with_additions), expected_m_1)
+})
+
+test_that("Matrix, format 1, uncompleted info", {
+  expect_equal(create_distance_matrix(df_format1_uncompleted),
+               expected_m_uncomplited)
+})
+
+test_that("Matrix, format 1, uncompleted info 2", {
+  expect_equal(create_distance_matrix(df_format1_uncompleted2),
+               expected_m_uncomplited2)
+})
+
+## TEST FOR FORMAT 2
+
+test_that("Matrix, format 2", {
+  expect_equal(create_distance_matrix(df_format2), expected_m_2)
+})
+
+test_that("Matrix uncompleted, format 2", {
+  expect_equal(create_distance_matrix(df_format2_uncompleted),
+               expected_m2_uncompleted)
+})
+
+test_that("Matrix, format 2, df with additions", {
+  expect_equal(create_distance_matrix(df_format2_with_additions), expected_m_2)
 })
